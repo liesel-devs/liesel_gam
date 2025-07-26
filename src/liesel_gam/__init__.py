2a380386940493127f000872b3483f72f99deb5a
wiep <wiep@users.noreply.github.com>
2025-07-26 07:56:50 +0200

    removes everything constructor

--- src/liesel_gam/__init__.py
+++ src/liesel_gam/__init__.py
@@ -1,6 +1,5 @@
 from .__about__ import __version__ as __version__
 from .builder.gam_builder import GamBuilder
-from .constructor import Constructor
 from .dist import MultivariateNormalSingular as MultivariateNormalSingular
 from .kernel import init_star_ig_gibbs as init_star_ig_gibbs
 from .kernel import star_ig_gibbs as star_ig_gibbs
@@ -12,7 +11,6 @@ from .var import SmoothTerm as SmoothTerm
 
 __all__ = [
     "__version__",
-    "Constructor",
     "GamBuilder",
     "MultivariateNormalSingular",
     "init_star_ig_gibbs",
