b29bfee0bfa5b77626a93e5096a0b85e7e7f4719
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2025-11-20 22:24:30 +0100

    export categorical functionality

--- src/liesel_gam/__init__.py
+++ src/liesel_gam/__init__.py
@@ -1,7 +1,9 @@
 from .__about__ import __version__ as __version__
 from .builder import BasisBuilder as BasisBuilder
+from .builder import CategoryMapping as CategoryMapping
 from .builder import TermBuilder as TermBuilder
 from .builder import VarIGPrior as VarIGPrior
+from .builder import series_is_categorical as series_is_categorical
 from .dist import MultivariateNormalSingular as MultivariateNormalSingular
 from .kernel import init_star_ig_gibbs as init_star_ig_gibbs
 from .kernel import star_ig_gibbs as star_ig_gibbs
--- src/liesel_gam/builder/__init__.py
+++ src/liesel_gam/builder/__init__.py
@@ -3,4 +3,6 @@
 from .builder import BasisBuilder as BasisBuilder
 from .builder import TermBuilder as TermBuilder
 from .builder import VarIGPrior as VarIGPrior
+from .category_mapping import CategoryMapping as CategoryMapping
+from .category_mapping import series_is_categorical as series_is_categorical
 from .registry import PandasRegistry as PandasRegistry
