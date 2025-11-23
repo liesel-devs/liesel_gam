096e252a63181a542e525eeb1fcbbb4192b9b242
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2025-11-23 02:17:37 +0100

    refactor CategoryMapping
    This refactoring makes the class behave as intended when the map attributes are changed after initialization.

--- src/liesel_gam/builder/category_mapping.py
+++ src/liesel_gam/builder/category_mapping.py
@@ -29,51 +29,9 @@ class CategoryMapping:
         self._label_for_unknown_code = None
 
         self.labels_to_integers_map = labels_to_integers_map
-
-        def labels_to_integers_fun(
-            x: np.typing.ArrayLike,
-        ) -> np.typing.NDArray[np.int_]:
-            x = np.asarray(x)
-            x_flat = x.flatten()
-            codes_flat = np.zeros_like(x_flat, dtype=int)
-
-            for i, xi in enumerate(x_flat):
-                codes_flat[i] = self.labels_to_integers_map.get(
-                    xi, self._code_for_unknown_label
-                )
-                if codes_flat[i] == self._code_for_unknown_label:
-                    raise UnknownLabelError(f"Category label {xi} is unknown.")
-
-            codes = np.reshape(codes_flat, shape=x.shape)
-
-            return np.astype(codes, np.int_)
-
-        self._labels_to_integers_fun = labels_to_integers_fun
-
         self.integers_to_labels_map = {
             code: label for label, code in self.labels_to_integers_map.items()
         }
-
-        def integers_to_labels_fun(
-            x: np.typing.ArrayLike,
-        ) -> np.typing.NDArray[np.int_]:
-            x = np.asarray(x)
-            x_flat = x.flatten()
-            labels_flat_list = []
-
-            for xi in x_flat:
-                label = self.integers_to_labels_map.get(
-                    xi, self._label_for_unknown_code
-                )
-                if label == self._label_for_unknown_code:
-                    raise UnknownCodeError(f"Category code {xi} is unknown.")
-                labels_flat_list.append(label)
-
-            labels_flat = np.asarray(labels_flat_list)
-            labels = np.reshape(labels_flat, shape=x.shape)
-            return labels
-
-        self._integers_to_labels_fun = integers_to_labels_fun
 
     @classmethod
     def from_series(cls, series: pd.Series | pd.Categorical) -> CategoryMapping:
@@ -96,24 +54,49 @@ class CategoryMapping:
         return cls(mapping)
 
     def labels_to_integers(
-        self, labels: np.typing.ArrayLike
+        self, labels: np.typing.ArrayLike | Sequence[str]
     ) -> np.typing.NDArray[np.int_]:
         """
         A function of labels -> integers.
 
         For unknown labels, returns -1.
         """
-        return self._labels_to_integers_fun(labels)
+        labels = np.asarray(labels)
+        labels_flat = labels.flatten()
+        codes_flat = np.zeros_like(labels_flat, dtype=int)
+
+        for i, xi in enumerate(labels_flat):
+            codes_flat[i] = self.labels_to_integers_map.get(
+                xi, self._code_for_unknown_label
+            )
+            if codes_flat[i] == self._code_for_unknown_label:
+                raise UnknownLabelError(f"Category label {xi} is unknown.")
+
+        codes = np.reshape(codes_flat, shape=labels.shape)
+
+        return np.astype(codes, np.int_)
 
     def integers_to_labels(
-        self, integers: np.typing.NDArray[np.int_] | Sequence[int]
+        self, integers: np.typing.ArrayLike | Sequence[int]
     ) -> np.typing.NDArray[Any]:
         """
         A function of integers -> labels.
 
         For integers without labels, returns
         """
-        return self._integers_to_labels_fun(integers)
+        integers = np.asarray(integers)
+        integers_flat = integers.flatten()
+        labels_flat_list = []
+
+        for xi in integers_flat:
+            label = self.integers_to_labels_map.get(xi, self._label_for_unknown_code)
+            if label == self._label_for_unknown_code:
+                raise UnknownCodeError(f"Category code {xi} is unknown.")
+            labels_flat_list.append(label)
+
+        labels_flat = np.asarray(labels_flat_list)
+        labels = np.reshape(labels_flat, shape=integers.shape)
+        return labels
 
 
 def series_is_categorical(series: pd.Series | pd.Categorical) -> bool:
