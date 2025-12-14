91276908872ef7139e5caff05aa2175e00f72f59
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2025-12-14 15:26:23 +0100

    organize exports

--- src/liesel_gam/__init__.py
+++ src/liesel_gam/__init__.py
@@ -3,6 +3,8 @@ from ryp import r, to_r
 
 from .__about__ import __version__ as __version__
 from .basis import Basis as Basis
+from .basis import LinBasis as LinBasis
+from .basis import MRFBasis as MRFBasis
 from .basis_builder import BasisBuilder as BasisBuilder
 from .category_mapping import CategoryMapping as CategoryMapping
 from .category_mapping import series_is_categorical as series_is_categorical
@@ -15,15 +17,15 @@ from .plots import plot_2d_smooth as plot_2d_smooth
 from .plots import plot_forest as plot_forest
 from .plots import plot_polys as plot_polys
 from .plots import plot_regions as plot_regions
-from .plots import polys_to_df as polys_to_df
-from .plots import summarise_1d_smooth as summarise_1d_smooth
-from .plots import summarise_1d_smooth_clustered as summarise_1d_smooth_clustered
-from .plots import summarise_cluster as summarise_cluster
-from .plots import summarise_lin as summarise_lin
-from .plots import summarise_nd_smooth as summarise_nd_smooth
-from .plots import summarise_regions as summarise_regions
 from .predictor import AdditivePredictor as AdditivePredictor
 from .registry import PandasRegistry as PandasRegistry
+from .summary import polys_to_df as polys_to_df
+from .summary import summarise_1d_smooth as summarise_1d_smooth
+from .summary import summarise_1d_smooth_clustered as summarise_1d_smooth_clustered
+from .summary import summarise_cluster as summarise_cluster
+from .summary import summarise_lin as summarise_lin
+from .summary import summarise_nd_smooth as summarise_nd_smooth
+from .summary import summarise_regions as summarise_regions
 from .term import BasisDot as BasisDot
 from .term import IndexingTerm as IndexingTerm
 from .term import Intercept as Intercept
@@ -31,6 +33,7 @@ from .term import LinTerm as LinTerm
 from .term import MRFTerm as MRFTerm
 from .term import RITerm as RITerm
 from .term import SmoothTerm as SmoothTerm
+from .term import StrctLinTerm as StrctLinTerm
 from .term import StrctTensorProdTerm as StrctTensorProdTerm
 from .term import StrctTerm as StrctTerm
 from .term_builder import TermBuilder as TermBuilder
