45dc73bc6abe2394df8d572251b5818aa2edaafc
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2026-03-12 16:13:51 +0100

    Update test_term_builder.py

--- tests/test_term_builder.py
+++ tests/test_term_builder.py
@@ -67,6 +67,7 @@ class TestTermBuilder:
         tb = gb.TermBuilder(registry)
         sx = tb.ps("x", k=10, prefix="test.")
         assert sx.name == "test.ps(x)"
+
     def test_init_scale(self, columb) -> None:
         tb = gb.TermBuilder.from_df(columb, prefix_names_by="test.")
         sx = tb.ps("x", k=10, scale=lsl.Var(1.0))
